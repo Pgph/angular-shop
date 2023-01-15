4c8a7dd231b969b430f778f9b1c735e99deb2647
patrykborkowskitukano <patryk.borkowski@tukano.com.pl>
2023-01-15 22:51:21 +0100

    added service with data

--- src/app/books/books.component.ts
+++ src/app/books/books.component.ts
@@ -1,52 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Book } from '../types/Book';
+import { BooksService } from './books.service';
 
-interface Book {
-  name: string;
-  author: string;
-  image: string;
-  amount: number;
-}
+
 
 @Component({
   selector: 'app-books',
   templateUrl: './books.component.html',
   styleUrls: ['./books.component.css']
 })
-export class BooksComponent {
+export class BooksComponent implements OnInit {
 
-  books: Book[] = [
-    {
-      name: 'Clean Code',
-      author: 'Robert C. Martin',
-      image: 'https://m.media-amazon.com/images/I/41xShlnTZTL._SX376_BO1,204,203,200_.jpg',
-      amount: 700,
-    },
-    {
-      name: 'Pragmatic Programmer',
-      author: 'Andrew Hunt',
-      image: 'https://m.media-amazon.com/images/I/51IA4hT6jrL._SX380_BO1,204,203,200_.jpg',    
-      amount: 800,
-    },
-    {
-      name: 'The Art of Computer Programming',
-      author: 'Donald John Fuller',
-      image: 'https://media.libris.to/jacket/39374455_the-art-of-computer-programming-volumes-1-4b-boxed-set.jpg',
-      amount: 1500,
-    },
-    {
-      name: 'Introduction to algorithms',
-      author: 't cormen',
-      image: 'https://m.media-amazon.com/images/I/513P8XoCAEL._SX376_BO1,204,203,200_.jpg',
-      amount: 1000,
-    }
-  ]
-  
-  
-  
-  isShowing: boolean = true;
+  books: Book[] = [];
 
-  toggleBooks() {
-    this.isShowing = !this.isShowing;
+  constructor(private booksService: BooksService) {}
+
+
+  card: Book[] = [];
+
+
+  ngOnInit(): void {
+    this.books = this.booksService.getBooks()
   }
 
+  addToCard(book: Book) {
+    console.log(book)
+  }
+
+  // addToCard(book: Book) {
+  //   this.card.push(book)
+  // }
 }
---
+++ src/app/books/books.service.ts
@@ -0,0 +1,38 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class BooksService {
+
+  constructor() {}
+
+  getBooks() {
+    return [
+      {
+        name: 'Clean Code',
+        author: 'Robert C. Martin',
+        image: 'https://m.media-amazon.com/images/I/41xShlnTZTL._SX376_BO1,204,203,200_.jpg',
+        amount: 700,
+      },
+      {
+        name: 'Pragmatic Programmer',
+        author: 'Andrew Hunt',
+        image: 'https://m.media-amazon.com/images/I/51IA4hT6jrL._SX380_BO1,204,203,200_.jpg',    
+        amount: 800,
+      },
+      {
+        name: 'The Art of Computer Programming',
+        author: 'Donald John Fuller',
+        image: 'https://media.libris.to/jacket/39374455_the-art-of-computer-programming-volumes-1-4b-boxed-set.jpg',
+        amount: 1500,
+      },
+      {
+        name: 'Introduction to algorithms',
+        author: 't cormen',
+        image: 'https://m.media-amazon.com/images/I/513P8XoCAEL._SX376_BO1,204,203,200_.jpg',
+        amount: 1000,
+      }
+    ]
+  }
+}
